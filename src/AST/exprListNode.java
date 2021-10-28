163ff3b8a81bb55bee59c0ba2e73161a60a95538
hyj542682306 <542682306@qq.com>
2021-10-28 14:47:15 +0800

    Create exprListNode.java

---
+++ src/AST/exprListNode.java
@@ -0,0 +1,18 @@
+package AST;
+
+import util.position;
+
+import java.util.ArrayList;
+
+public class exprListNode extends exprNode {
+    public ArrayList<exprNode> exprList = new ArrayList<>();
+
+    public exprListNode(position _pos) {
+        super(_pos);
+    }
+
+    @Override
+    public void accept(ASTvisitor visitor) {
+        visitor.visit(this);
+    }
+}
