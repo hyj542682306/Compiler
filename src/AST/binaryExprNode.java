a37c932ec82e8a82b9d45f8d05ec69d45e0df08c
hyj542682306 <542682306@qq.com>
2021-10-28 14:46:57 +0800

    Create binaryExprNode.java

---
+++ src/AST/binaryExprNode.java
@@ -0,0 +1,20 @@
+package AST;
+
+import util.position;
+
+public class binaryExprNode extends exprNode {
+    public exprNode expr1, expr2;
+    public String op;
+
+    public binaryExprNode(position _pos, exprNode _expr1, exprNode _expr2, String _op) {
+        super(_pos);
+        expr1 = _expr1;
+        expr2 = _expr2;
+        op = _op;
+    }
+
+    @Override
+    public void accept(ASTvisitor visitor) {
+        visitor.visit(this);
+    }
+}
