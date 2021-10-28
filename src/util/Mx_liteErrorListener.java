b8c1762191557c188ce0034247c4b8df076b40f8
hyj542682306 <542682306@qq.com>
2021-10-28 14:49:34 +0800

    Create Mx_liteErrorListener.java

---
+++ src/util/Mx_liteErrorListener.java
@@ -0,0 +1,19 @@
+package Util;
+
+import util.error.syntaxError;
+import util.position;
+import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+
+public class Mx_liteErrorListener extends BaseErrorListener {
+    @Override
+    public void syntaxError(Recognizer<?, ?> recognizer,
+                            Object offendingSymbol,
+                            int line, int charPositionInLine,
+                            String msg,
+                            RecognitionException e) {
+
+        throw new syntaxError(msg, new position(line, charPositionInLine));
+    }
+}
