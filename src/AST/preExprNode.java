590794d3d1136fb74ff6cda7a249c1419d14ce19
hyj542682306 <542682306@qq.com>
2021-10-28 14:47:41 +0800

    Create preExprNode.java

---
+++ src/AST/preExprNode.java
@@ -0,0 +1,19 @@
+package AST;
+
+import util.position;
+
+public class preExprNode extends exprNode {
+    public String op;
+    public exprNode expr;
+
+    public preExprNode(position _pos, String _op, exprNode _expr) {
+        super(_pos);
+        op = _op;
+        expr = _expr;
+    }
+
+    @Override
+    public void accept(ASTvisitor visitor) {
+        visitor.visit(this);
+    }
+}
