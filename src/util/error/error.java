bf4c016b85bcceab9dd3138ba8c048357e0e6d24
hyj542682306 <542682306@qq.com>
2021-10-28 14:49:19 +0800

    Create error.java

---
+++ src/util/error/error.java
@@ -0,0 +1,17 @@
+package util.error;
+
+import util.position;
+
+abstract public class error extends RuntimeException {
+    private position pos;
+    private String message;
+
+    public error(String msg, position pos) {
+        this.pos = pos;
+        this.message = msg;
+    }
+
+    public String toString() {
+        return message + ": " + pos.toString();
+    }
+}
