f6b1a94b5baebe92ed52bff8799208e2cc288ddd
hyj542682306 <542682306@qq.com>
2021-10-28 14:48:15 +0800

    Create varDecStmtNode.java

---
+++ src/AST/varDecStmtNode.java
@@ -0,0 +1,20 @@
+package AST;
+
+import util.position;
+
+public class varDecStmtNode extends stmtNode {
+    public typeNode type;
+    public String id;
+    public exprNode expr;
+
+    public varDecStmtNode(position _pos, String _id, exprNode _expr) {
+        super(_pos);
+        id = _id;
+        expr = _expr;
+    }
+
+    @Override
+    public void accept(ASTvisitor visitor) {
+        visitor.visit(this);
+    }
+}
