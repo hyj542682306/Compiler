3f7c8aab5726c05e5cf715a35bb1f30ab0e9272e
hyj542682306 <542682306@qq.com>
2021-10-28 14:46:55 +0800

    Create ASTvisitor.java

---
+++ src/AST/ASTvisitor.java
@@ -0,0 +1,63 @@
+package AST;
+
+public interface ASTvisitor {
+    void visit(progNode it);
+
+    void visit(typeNode it);
+
+    void visit(funcDefNode it);
+
+    void visit(classDefNode it);
+
+    void visit(varDecStmtNode it);
+
+    void visit(varDefStmtNode it);
+
+    void visit(suiteStmtNode it);
+
+    void visit(ifStmtNode it);
+
+    void visit(whileStmtNode it);
+
+    void visit(forStmtNode it);
+
+    void visit(returnStmtNode it);
+
+    void visit(breakStmtNode it);
+
+    void visit(continueStmtNode it);
+
+    void visit(pureExprStmtNode it);
+
+    void visit(emptyStmtNode it);
+
+    void visit(intExprNode it);
+
+    void visit(boolExprNode it);
+
+    void visit(stringExprNode it);
+
+    void visit(nullExprNode it);
+
+    void visit(thisExprNode it);
+
+    void visit(idExprNode it);
+
+    void visit(lambdaExprNode it);
+
+    void visit(funcExprNode it);
+
+    void visit(indexExprNode it);
+
+    void visit(classExprNode it);
+
+    void visit(sufExprNode it);
+
+    void visit(preExprNode it);
+
+    void visit(newExprNode it);
+
+    void visit(binaryExprNode it);
+
+    void visit(exprListNode it);
+}
