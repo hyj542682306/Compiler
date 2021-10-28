bcb89f266ba08e5b6484acdd5df2fdd2dfe75697
hyj542682306 <542682306@qq.com>
2021-10-28 14:47:29 +0800

    Create indexExprNode.java

---
+++ src/AST/indexExprNode.java
@@ -0,0 +1,19 @@
+package AST;
+
+import util.position;
+
+public class indexExprNode extends exprNode {
+    public exprNode bas;
+    public exprNode off;
+
+    public indexExprNode(position _pos, exprNode _bas, exprNode _off) {
+        super(_pos);
+        bas = _bas;
+        off = _off;
+    }
+
+    @Override
+    public void accept(ASTvisitor visitor) {
+        visitor.visit(this);
+    }
+}
