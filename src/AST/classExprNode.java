bf008ad6fd9b7fd43422dbc5149ddb15553c61ea
hyj542682306 <542682306@qq.com>
2021-10-28 14:47:06 +0800

    Create classExprNode.java

---
+++ src/AST/classExprNode.java
@@ -0,0 +1,19 @@
+package AST;
+
+import util.position;
+
+public class classExprNode extends exprNode {
+    public exprNode name;
+    public String id;
+
+    public classExprNode(position _pos, exprNode _name, String _id) {
+        super(_pos);
+        name = _name;
+        id = _id;
+    }
+
+    @Override
+    public void accept(ASTvisitor visitor) {
+        visitor.visit(this);
+    }
+}
