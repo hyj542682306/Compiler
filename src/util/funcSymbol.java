b0e852708525a938dce9e89ba4344224044a5743
hyj542682306 <542682306@qq.com>
2021-10-29 23:29:04 +0800

    Update funcSymbol.java

--- src/util/funcSymbol.java
+++ src/util/funcSymbol.java
@@ -2,7 +2,7 @@ package util;
 
 import java.util.ArrayList;
 
-public class funcSymbol {
+public class funcSymbol extends Type {
     public Type type = null;
     public String id;
     public ArrayList<varSymbol> paraList = new ArrayList<>();
