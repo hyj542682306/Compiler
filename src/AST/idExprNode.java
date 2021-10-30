0876175b15087fb16371910f54d18ec6041715e6
hyj542682306 <542682306@qq.com>
2021-10-30 11:54:12 +0800

    Update idExprNode.java

--- src/AST/idExprNode.java
+++ src/AST/idExprNode.java
@@ -6,7 +6,7 @@ public class idExprNode extends exprNode {
     public String id;
 
     public idExprNode(position _pos, String _id) {
-        super(_pos);
+        super(_pos,true);
         id = _id;
     }
 
