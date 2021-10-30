804c44c815eac20d5a8c5438e435f895f9540917
hyj542682306 <542682306@qq.com>
2021-10-30 16:08:16 +0800

    Create funcDefNode.java

---
+++ src/AST/defNode/funcDefNode.java
@@ -0,0 +1,28 @@
+package AST.defNode;
+
+import AST.*;
+import AST.exprNode.*;
+import AST.stmtNode.*;
+import util.position;
+
+import java.util.ArrayList;
+
+public class funcDefNode extends defNode {
+    public typeNode type;
+    public String id;
+    public ArrayList<varDecStmtNode> paraList = new ArrayList<>();
+    public suiteStmtNode suite;
+
+    public funcDefNode(position _pos, typeNode _type, String _id, ArrayList<varDecStmtNode> _paraList, suiteStmtNode _suite) {
+        super(_pos);
+        type = _type;
+        id = _id;
+        paraList = _paraList;
+        suite = _suite;
+    }
+
+    @Override
+    public void accept(ASTvisitor visitor) {
+        visitor.visit(this);
+    }
+}
